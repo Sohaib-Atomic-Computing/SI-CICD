3104e22378f26885031d65999b6c797d0a76f64c
Mahmoud_Shtayeh <mahmoud.shetia@satra.io>
2022-06-28 16:54:57 +0200

    Add User service on core module
    [DAC-1026]

--- Core/src/main/java/services/UserService.java
+++ Core/src/main/java/services/UserService.java
@@ -1,2 +1,13 @@
-package services;public class UserService {
+package services;
+
+import org.springframework.stereotype.Service;
+import repositories.UserRepository;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
