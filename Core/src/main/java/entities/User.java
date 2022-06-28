6989e3e030a8297ad89c017e5df44fc3c761bec9
Mahmoud_Shtayeh <mahmoud.shetia@satra.io>
2022-06-28 14:50:02 +0200

    Create User entity on Core module
    [DAC-1026]

---
+++ Core/src/main/java/entities/User.java
@@ -0,0 +1,44 @@
+package entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_seq",
+            sequenceName = "user_seq"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_seq"
+    )
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @Column(nullable = false)
+    private String mobile;
+
+    private Boolean enabled;
+
+    public static void main(String[] args) {
+
+    }
+}
