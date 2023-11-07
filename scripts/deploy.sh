4440a5137a66d56fcf8a31d923a805061b802bed
Sohaib Ismail <sohaib.ismail@atomic-computing.com>
2023-11-07 16:00:43 +0500

    updated scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "[Docker Compose] Building Docker images and starting containers..."
-docker-compose -f /home/iconnect/iconnect-main/docker-compose.yml up --build -d
+docker-compose up --build -d
--- scripts/stop-application.sh
+++ scripts/stop-application.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "[Stop Application] Stopping Docker containers..."
-docker-compose -f /home/iconnect/iconnect-main/docker-compose.yml down
+docker-compose down
