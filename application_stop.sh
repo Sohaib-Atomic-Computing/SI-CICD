278c3bc0db93a9df88109e87734a98e0b4d82093
Sohaib Ismail <sohaib.ismail@atomic-computing.com>
2023-11-12 20:29:04 +0500

    removed extra scripts

--- application_stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo $(pwd)
-echo "[Stop Application] Stopping Docker containers..."
-docker-compose down --volumes
--- validate_service.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "[ValidateService] All the services are running properly"
