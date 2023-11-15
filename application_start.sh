2e29e775e6792b09ed3176364dc165cf17a65d50
Sohaib Ismail <sohaib.ismail@atomic-computing.com>
2023-11-15 14:40:02 +0500

    updated docker-compose file and application_start script

--- application_start.sh
+++ application_start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Fetch secrets from AWS Secrets Manager
 secret_arn="arn:aws:secretsmanager:eu-west-1:037680133179:secret:SIEnvSecrets-1lPfo9"
 aws_region="eu-west-1"
 
 echo "[Fetching Secrets] Fetching secrets from AWS Secrets Manager..."
 secrets=$(aws secretsmanager get-secret-value --secret-id $secret_arn --region $aws_region --output json | jq -r ".SecretString")
 
-# Print the fetched secrets
 echo "[Fetched Secrets] Fetched secrets:"
 echo "$secrets"
 
@@ -23,8 +21,5 @@ cd /home/iconnect/iconnect-main/
 echo "[Docker Compose] Building Docker images and starting containers..."
 docker-compose up --build -d
 
-# Other tasks related to starting your application
-# ...
-
 # Continue with your existing startup logic (if not included in this script)
-# exec /path/to/your_existing_start_application.sh
+exec /home/iconnect/iconnect-main/start_application.sh
